f71f158a4e2eeeff92fcbff5423f1658296b4caf
Justin Li <justinnhli@gmail.com>
2023-01-21 12:41:42 -0800

    clean up, lint, and add type annotations

--- firekeeper.py
+++ firekeeper.py
@@ -2,138 +2,45 @@
 
 import re
 import sqlite3
-from time import sleep
-from random import random, shuffle
 from argparse import ArgumentParser
 from enum import Enum
 from json import load as json_load, dump as json_dump
 from pathlib import Path
 from platform import system
+from random import random, shuffle
 from shutil import copyfile
 from subprocess import run as run_subprocess
 from tempfile import TemporaryDirectory
+from time import sleep
+from typing import Any, Optional
 from urllib.parse import urlsplit
 
 ARCHIVE_PATH = Path('~/media/web-archive').expanduser().resolve()
 
 
-class Status(Enum):
-    # FIXME should use StrEnum from Python 3.11 instead
-    # which allows for {'accepted': True}[Status.ACCEPTED]
-    REJECTED = 'rejected'
-    UNSORTED = 'unsorted'
-    ACCEPTED = 'accepted'
-    ARCHIVED = 'archived'
+class Status(str):
+    pass
 
 
-REJECTED, UNSORTED, ACCEPTED, ARCHIVED = Status
-REJECTED = 'rejected'
-UNSORTED = 'unsorted'
-ACCEPTED = 'accepted'
-ARCHIVED = 'archived'
+REJECTED = Status('rejected')
+UNSORTED = Status('unsorted')
+ACCEPTED = Status('accepted')
+ARCHIVED = Status('archived')
 
 
-def get_profile():
-    # type: () -> Path
-    if system() == 'Linux':
-        profiles_path = Path('~/.mozilla/firefox')
-    else:
-        profiles_path = Path('~/Library/Application Support/Firefox/Profiles')
-    return max(
-        (
-            child for child in profiles_path.expanduser().resolve().iterdir()
-            if child.is_dir() and '.' in child.name
-        ),
-        key=(lambda path: path.stat().st_mtime),
-    )
-
-
-def get_places_db(profile):
-    db_path = profile / 'places.sqlite'
-    assert db_path.exists()
-    return db_path
-
-
-def get_history(profile=None):
-    if profile is None:
-        profile = get_profile()
-    db_path = get_places_db(profile)
-    # make a copy of the database to avoid locks
-    urls = set()
-    with TemporaryDirectory() as temp_dir:
-        temp_db = Path(temp_dir) / 'temp.sqlite'
-        copyfile(db_path, temp_db)
-        con = sqlite3.connect(temp_db)
-        cur = con.cursor()
-        for row, in cur.execute('SELECT DISTINCT url FROM moz_places'):
-            urls.add(row)
-    return urls
-
-
-class URL:
-
-    def __init__(self, url):
-        self.url = url
-        _, self.netloc, self.path, *_ = urlsplit(url)
-        self.domain = '.'.join(self.netloc.split('.')[-2:])
-
-    def __str__(self):
-        return self.url
-
-    def __hash__(self):
-        return hash(self.url)
-
-class URLSet:
-
-    class Node:
-
-        def __init__(self, end=False):
-            self.end = end
-            self.children = {}
-
-        def __getitem__(self, key):
-            return self.children[key]
-
-        def __setitem__(self, key, value):
-            self.children[key] = value
-
-        def __contains__(self, part):
-            return part in self.children
-
-    def __init__(self, urls):
-        self.root = URLSet.Node()
-        for url in urls:
-            self.add(url)
-
-    def add(self, url):
-        node = self.root
-        for part in url.parts:
-            if part not in node:
-                node[part] = URLSet.Node()
-            node = node[part]
-        node.end = True
-
-
-    def discard(self, url):
-        pass # FIXME
-
-
-def read_urls():
-    with open('urls', encoding='utf-8') as fd:
-        return {status: set(urls) for status, urls in json_load(fd).items()}
-
-
-def write_urls(cache):
-    cache_obj = {status: sorted(urls) for status, urls in cache.items()}
-    with open('urls', 'w', encoding='utf-8') as fd:
-        json_dump(cache_obj, fd)
+class URL(str):
+    pass
 
 
 class Rule:
 
     def __init__(self, rule_string):
+        # type: (str) -> None
         self.rule_string = rule_string
-        match = re.fullmatch('(?P<preempt>!?)(?P<scheme>([a-z]*:)?)//(?P<netloc>[^/]*)/(?P<path>.*)', rule_string.strip())
+        match = re.fullmatch(
+            '(?P<preempt>!?)(?P<scheme>([a-z]*:)?)//(?P<netloc>[^/]*)/(?P<path>.*)',
+            rule_string.strip(),
+        )
         if not match:
             raise ValueError(f'rule does not contain the required pattern: {rule_string}')
         self.preempt = bool(match.group('preempt'))
@@ -141,7 +48,16 @@ class Rule:
         self.netloc_regex = re.compile(f'{match.group("netloc")}$', flags=re.IGNORECASE)
         self.path_regex = re.compile(f'^{match.group("path")}', flags=re.IGNORECASE)
 
+    def __eq__(self, other):
+        # type: (Any) -> bool
+        return self.rule_string == other.rule_string
+
+    def __hash__(self):
+        # type: () -> int
+        return hash(self.rule_string)
+
     def __str__(self):
+        # type: () -> str
         if self.preempt:
             preempt_pattern = '!'
         else:
@@ -160,21 +76,41 @@ class Rule:
         return result
 
     def __repr__(self):
+        # type: () -> str
         return f'Rule("{self.rule_string}")'
 
     def matches(self, url):
+        # type: (URL) -> bool
         parts = urlsplit(url)
         scheme, netloc, path, *_ = parts
         if parts.port:
             netloc = netloc[:-len(str(parts.port)) - 1]
-        return (
+        return bool(
             self.scheme_regex.search(scheme)
             and self.netloc_regex.search(netloc)
             and self.path_regex.search(path.lstrip('/'))
         )
 
 
+Cache = dict[Status, set[URL]]
+RuleBook = dict[Status, list[Rule]]
+
+
+def read_urls():
+    # type: () -> Cache
+    with open('urls', encoding='utf-8') as fd:
+        return {status: set(urls) for status, urls in json_load(fd).items()}
+
+
+def write_urls(cache):
+    # type: (Cache) -> None
+    cache_obj = {status: sorted(urls) for status, urls in cache.items()}
+    with open('urls', 'w', encoding='utf-8') as fd:
+        json_dump(cache_obj, fd)
+
+
 def read_rules():
+    # type: () -> RuleBook
     with open('rules', encoding='utf-8') as fd:
         return {
             status: [Rule(rule) for rule in rules]
@@ -183,20 +119,62 @@ def read_rules():
 
 
 def write_rules(rules):
-    rules_obj = {REJECTED: [], ACCEPTED: []}
+    # type: (RuleBook) -> None
+    rules_obj = {REJECTED: [], ACCEPTED: []} # type: dict[Status, list[str]]
     for status, ruleset in rules.items():
         rules_obj[status] = sorted(str(rule) for rule in ruleset)
     with open('rules', 'w', encoding='utf-8') as fd:
         json_dump(rules_obj, fd)
 
 
+def get_profile():
+    # type: () -> Path
+    if system() == 'Linux':
+        profiles_path = Path('~/.mozilla/firefox')
+    else:
+        profiles_path = Path('~/Library/Application Support/Firefox/Profiles')
+    return max(
+        (
+            child for child in profiles_path.expanduser().resolve().iterdir()
+            if child.is_dir() and '.' in child.name
+        ),
+        key=(lambda path: path.stat().st_mtime),
+    )
+
+
+def get_places_db(profile):
+    # type: (Path) -> Path
+    db_path = profile / 'places.sqlite'
+    assert db_path.exists()
+    return db_path
+
+
+def get_history(profile=None):
+    # type: (Optional[Path]) -> set[URL]
+    if profile is None:
+        profile = get_profile()
+    db_path = get_places_db(profile)
+    # make a copy of the database to avoid locks
+    urls = set()
+    with TemporaryDirectory() as temp_dir:
+        temp_db = Path(temp_dir) / 'temp.sqlite'
+        copyfile(db_path, temp_db)
+        con = sqlite3.connect(temp_db)
+        cur = con.cursor()
+        for row, in cur.execute('SELECT DISTINCT url FROM moz_places'):
+            urls.add(row)
+    return urls
+
+
 def add_history_to_cache(cache):
+    # type: (Cache) -> None
     history = get_history()
     new_urls = history - set().union(*cache.values())
-    cache[UNSORTED].update(new_urls)
+    cache[UNSORTED].update(history - set().union(*cache.values()))
 
 
 def process_url(url, rules):
+    # type: (URL, RuleBook) -> Status
     rejected = False
     for rule in rules.get(REJECTED, []):
         if rule.matches(url):
@@ -213,6 +191,7 @@ def process_url(url, rules):
 
 
 def process_urls(cache, rules=None, from_status=UNSORTED):
+    # type: (Cache, Optional[RuleBook], Status) -> None
     if rules is None:
         rules = read_rules()
     for url in list(cache[from_status]):
@@ -226,6 +205,7 @@ def process_urls(cache, rules=None, from_status=UNSORTED):
 
 
 def get_archive_path(url):
+    # type: (URL) -> Path
     parts = urlsplit(url)
     _, netloc, path, *_ = parts
     domain = '.'.join(netloc.split('.')[-2:])
@@ -236,6 +216,7 @@ def get_archive_path(url):
 
 
 def archive_url(url):
+    # type: (URL) -> None
     archive_path = get_archive_path(url)
     archive_path.parent.mkdir(parents=True, exist_ok=True)
     command = [
@@ -253,6 +234,7 @@ def archive_url(url):
 
 
 def lint_conflicting_rules(cache, rules):
+    # type: (Cache, RuleBook) -> None
     for status, urls in cache.items():
         for url in urls:
             matches = {}
@@ -265,19 +247,22 @@ def lint_conflicting_rules(cache, rules):
 
 
 def lint_verify_archive(cache, _):
+    # type: (Cache, RuleBook) -> None
     for url in cache[ARCHIVED]:
         path = get_archive_path(url)
         if not path.exists():
             print(f'URL archive missing: {url}')
 
 
-def lint(cache):
+def do_lint(cache):
+    # type: (Cache) -> None
     rules = read_rules()
     lint_conflicting_rules(cache, rules)
     lint_verify_archive(cache, rules)
 
 
 def do_status(cache):
+    # type: (Cache) -> None
     rejected = len(cache[REJECTED])
     unsorted = len(cache[UNSORTED])
     accepted = len(cache[ACCEPTED])
@@ -292,6 +277,7 @@ def do_status(cache):
 
 
 def do_reset(cache):
+    # type: (Cache) -> None
     # reset the entire cache
     cache = {
         REJECTED: set(),
@@ -324,32 +310,28 @@ def do_reset(cache):
 
 
 def do_import(cache):
+    # type: (Cache) -> None
     add_history_to_cache(cache)
     write_urls(cache)
     process_urls(cache)
     write_urls(cache)
 
 
-def do_archive(cache, limit=10):
+def do_archive(cache, limit=-1):
+    # type: (Cache, int) -> None
     urls = list(cache[ACCEPTED])
-    #urls = [url for url in urls if 'washingtonpost' in url]
     shuffle(urls)
-    if limit:
+    if limit > 0:
         urls = urls[:limit]
     for url in urls:
         archive_url(url)
-        sleep(1 + random())
         cache[ACCEPTED].discard(url)
         cache[ARCHIVED].add(url)
-
-
-def do_lint(cache):
-    # check for redundant rules
-    # list conflicting URLs
-    pass
+        sleep(1 + random())
 
 
 def main():
+    # type: () -> None
     arg_parser = ArgumentParser()
     arg_parser.add_argument(
         'action',
